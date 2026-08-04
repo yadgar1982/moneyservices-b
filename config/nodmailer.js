cd7e52cdb6b20ee302a89f050cce0a0f5f101d34
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-08-04 19:07:49 -0400

    nodmail gmail stmp is added

--- config/nodmailer.js
+++ config/nodmailer.js
@@ -1,18 +1,20 @@
 import nodemailer from "nodemailer";
 
-export const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
 
-  // Test SMTP connection
-  await transporter.verify();
-
-  // Send email
+export const sendEmail = ({ to, subject, html }) => {
   return transporter.sendMail({
     from: `"Money Services" <${process.env.EMAIL_USER}>`,
     to,
