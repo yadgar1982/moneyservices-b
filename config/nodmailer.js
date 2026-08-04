fab153332ce3d995a375117d7c9c31ef086169e1
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-08-04 19:12:16 -0400

    nodmailer updated

--- config/nodmailer.js
+++ config/nodmailer.js
@@ -1,20 +1,18 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
+export const sendEmail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-export const sendEmail = ({ to, subject, html }) => {
+  // Test SMTP connection
+  await transporter.verify();
+
+  // Send email
   return transporter.sendMail({
     from: `"Money Services" <${process.env.EMAIL_USER}>`,
     to,
